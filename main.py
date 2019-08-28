9cf6e59ea566c72140f2e2e57f9f22f86afbe445
송기원 <kiwon0319@gmail.com>
2019-08-29 01:05:52 +0900

    .

--- main.py
+++ main.py
@@ -9,43 +9,26 @@ except FileNotFoundError as e:
     print("original.gun파일을 찾을 수 없습니다.")
     exit()
 
-id = data ["gun1"] ["gun_id"]
-lv = data ["gun1"] ["gun_level"]
+for key in data:
 
-stat = CalcStat.CalcStat(int(id), int(lv)).calc()
+    if key == "BossHP":
+        continue
+    if key == "enemyGroupID":
+        continue
 
-data ["gun1"] ["life"] = stat ["hp"] * int (data["gun1"] ["number"])
-data ["gun1"] ["pow"] = stat ["pow"]
-data ["gun1"] ["hit"] = stat ["hit"]
-data ["gun1"] ["dodge"] = stat ["dodge"]
-data ["gun1"] ["rate"] = stat ["rate"]
+    print (key)
 
-data ["gun2"] ["life"] = stat ["hp"] * int (data["gun2"] ["number"])
-data ["gun2"] ["pow"] = stat ["pow"]
-data ["gun2"] ["hit"] = stat ["hit"]
-data ["gun2"] ["dodge"] = stat ["dodge"]
-data ["gun2"] ["rate"] = stat ["rate"]
+    id = data [key] ["gun_id"]
+    lv = data [key] ["gun_level"]
 
-data ["gun3"] ["life"] = stat ["hp"] * int (data["gun3"] ["number"])
-data ["gun3"] ["pow"] = stat ["pow"]
-data ["gun3"] ["hit"] = stat ["hit"]
-data ["gun3"] ["dodge"] = stat ["dodge"]
-data ["gun3"] ["rate"] = stat ["rate"]
+    stat = CalcStat.CalcStat(int(id), int(lv)).calc()
 
-data ["gun4"] ["life"] = stat ["hp"] * int (data["gun4"] ["number"])
-data ["gun4"] ["pow"] = stat ["pow"]
-data ["gun4"] ["hit"] = stat ["hit"]
-data ["gun4"] ["dodge"] = stat ["dodge"]
-data ["gun4"] ["rate"] = stat ["rate"]
+    data [key] ["life"] = stat ["hp"] * int (data[key] ["number"])
+    data [key] ["pow"] = stat ["pow"]
+    data [key] ["hit"] = stat ["hit"]
+    data [key] ["dodge"] = stat ["dodge"]
+    data [key] ["rate"] = stat ["rate"]
 
-data ["gun5"] ["life"] = stat ["hp"] * int (data["gun5"] ["number"])
-data ["gun5"] ["pow"] = stat ["pow"]
-data ["gun5"] ["hit"] = stat ["hit"]
-data ["gun5"] ["dodge"] = stat ["dodge"]
-data ["gun5"] ["rate"] = stat ["rate"]
-
-print (data["gun1"])
-print (data["gun2"])
 
 
 def data_to_json(data) :
